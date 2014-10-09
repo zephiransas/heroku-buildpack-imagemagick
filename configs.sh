eb9aace390ac309f4affe1b69d90818fbbb3a723
Takafumi Yoshida <zephiransas@gmail.com>
2014-10-09 11:19:50 +0900

    Bump to Image Magick 6.8.9-8

--- configs.sh
+++ configs.sh
@@ -1,6 +1,6 @@
 
-export LIBPNG_VERSION="1.5.14"
-export IMAGE_MAGICK_VERSION="6.8.9-5"
-export REMOTE_PATH="imagemagick-${IMAGE_MAGICK_VERSION}.tgz"
-export LIBPNG_REMOTE_PATH="libpng-${LIBPNG_VERSION}.tgz"
-export S3_BUCKET="buildpack-imagemagick"
+export LIBPNG_VERSION="1.6.13"
+export IMAGE_MAGICK_VERSION="6.8.9-8"
+export REMOTE_PATH="ImageMagick-${IMAGE_MAGICK_VERSION}.tar"
+export LIBPNG_REMOTE_PATH="libpng-${LIBPNG_VERSION}.tar"
+export S3_BUCKET="build-imagemagick"
